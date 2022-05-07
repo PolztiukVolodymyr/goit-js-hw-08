5d8f7109f0754e9bea4bffcabed1997f5ad3c489
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2022-05-07 22:32:17 +0300

    task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import vimeoPlayer from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new vimeoPlayer(iframe);
+
+
+const onPlay = function(data) {
+
+ player.setCurrentTime(Number(localStorage.getItem("videoplayer-current-time")))
+
+};
+
+const onTimeUpdate = function(data) {
+
+localStorage.setItem("videoplayer-current-time", data.seconds);
+
+};
+
+player.on('play', onPlay);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+
+// console.log(iframe);
+// console.log(player);
