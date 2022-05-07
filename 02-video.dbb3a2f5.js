2795fee21a7e5bfd2da2b92c1883241091412612
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2022-05-07 20:09:44 +0000

    Deploying to gh-pages from @ PolztiukVolodymyr/goit-js-hw-08@52ff189cfee2210c21a2316c136acd542f02d31c 🚀

--- 02-video.dbb3a2f5.js
+++ 02-video.dbb3a2f5.js
@@ -7,4 +7,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=r(require("@vimeo/player")),t=r(require("lodash.throttle"));function r(e){return e&&e.__esModule?e:{default:e}}const o=document.querySelector("iframe"),u=new e.default(o),n=function(e){u.setCurrentTime(Number(localStorage.getItem("videoplayer-current-time")))},a=function(e){localStorage.setItem("videoplayer-current-time",e.seconds)};u.on("play",n),u.on("timeupdate",(0,t.default)(a,1e3));
 },{"@vimeo/player":"zU8P","lodash.throttle":"FNyO"}]},{},["iiAF"], null)
-//# sourceMappingURL=/parcel-project-template/02-video.0c0a9ec8.js.map
+//# sourceMappingURL=/parcel-project-template/02-video.dbb3a2f5.js.map
